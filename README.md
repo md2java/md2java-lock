235ce420b5a3ee6a3f776fe14b6b136c58807860
mohd <mohammad2java@gmail.com>
2023-10-24 21:18:51 +0530

    updated for postgres

--- README.md
+++ README.md
@@ -17,15 +17,24 @@
 			<artifactId>md2java-lock</artifactId>
 			<version>1.0.0-SNAPSHOT</version>
 		</dependency>
-
+			
 	2) apply EnableXXX
 	 @EnableClusterLock(monitorAt = 45*1000,updateAt = 30*1000)
 	 here monitorAt --background job keep checking activenode is working fine
 	  and updateAt --background job keep updating on lockprovider about lastrun.
+	  
+	3)create bean of lockprovider- supported lockprovider is JdbcLockProvider [h2,mysql,postgresql,sqlserver],
 	
-	3) apply ClusterLock on method[wherever you needed it]
+	@Bean
+	public LockProvider lockProvider(DataSource dataSource) {
+		JdbcLockProvider jdbcLockProvider = new JdbcLockProvider(dataSource);
+		return jdbcLockProvider;
+	}
+	
+	4) apply ClusterLock on method[wherever you needed it]
 	  @ClusterLock(name = "logTest11") 
 	  here name must be unique
 	  
+	
 	
 	
--- src/main/java/io/github/md2java/lock/util/QueryList.java
+++ src/main/java/io/github/md2java/lock/util/QueryList.java
@@ -3,19 +3,19 @@ package io.github.md2java.lock.util;
 public enum QueryList {
 
 	H2("CREATE TABLE CLUSTERLOCKINFO (Id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),lastrun TIMESTAMP,activenode VARCHAR(255));"),
-	MYSQL("CREATE TABLE CLUSTERLOCKINFO (Id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),lastrun TIMESTAMP,activenode VARCHAR(255));")
-	;
-   
+	MYSQL("CREATE TABLE CLUSTERLOCKINFO (Id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),lastrun TIMESTAMP,activenode VARCHAR(255));"),
+	POSTGRE("CREATE TABLE CLUSTERLOCKINFO (Id SERIAL PRIMARY KEY,name VARCHAR(255),lastrun TIMESTAMP,activenode VARCHAR(255));"),
+	SQLSERVER("CREATE TABLE CLUSTERLOCKINFO (Id INT IDENTITY(1,1) PRIMARY KEY,name NVARCHAR(255),lastrun DATETIME,activenode NVARCHAR(255));");
+
 	private String query;
-	
+
 	private QueryList(String query) {
 		this.query = query;
-		
+
 	}
 
 	public String query() {
 		return query;
 	}
-	
-    
+
 }
